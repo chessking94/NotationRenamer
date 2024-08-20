fe7d2cc3a6f2fc637b8b187f0d6c0ae19f4e1459
Ethan Hunt <chessking94@gmail.com>
2024-08-19 22:06:47 -0500

    Implementation

---
+++ NotationRenamer/MainWindow.xaml.vb
@@ -0,0 +1,162 @@
+﻿Imports System.Data
+Imports System.IO
+Imports Microsoft.Data.SqlClient
+
+Class MainWindow
+    Private myConfig As New Utilities_NetCore.clsConfig
+    Private db_Connection As New SqlConnection
+    Private savePathDefault As String
+    Private toBeFormattedDefaultPath As String
+
+    Private Sub WindowLoaded() Handles Me.Loaded, btn_Reset.Click
+        Dim projectDir As String = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\.."))
+        Dim configFile As String = Path.Combine(projectDir, "appsettings.json")
+        myConfig.configFile = configFile
+
+        Dim savePathDefault As String = Path.Combine(myConfig.getConfig("savePathRootDefault"), Date.Today.Year)
+        Dim toBeFormattedDefaultPath As String = myConfig.getConfig("toBeFormattedPathDefault")
+
+#If DEBUG Then
+        Dim connectionString As String = myConfig.getConfig("connectionStringDev")
+#Else
+        Dim connectionString As String = myConfig.getConfig("connectionStringProd")
+#End If
+        db_Connection = Utilities_NetCore.Connection(connectionString)
+
+        'set initial states
+        dp_StartDate.SelectedDate = Nothing
+        dp_EndDate.SelectedDate = Nothing
+        tb_RenamePath.Text = toBeFormattedDefaultPath
+        tb_SavePath.Text = savePathDefault
+        btn_Rename.IsEnabled = False
+
+        'hide query
+        dg_GameData.ItemsSource = Nothing
+        dg_GameData.Visibility = Visibility.Hidden
+    End Sub
+
+    Private Sub RefreshDataGrid() Handles btn_Refresh.Click
+        'validation
+        Dim validationFailReason As String = ""
+        If validationFailReason = "" Then
+            If dp_StartDate.SelectedDate Is Nothing Then
+                validationFailReason = "Missing Start Date"
+            End If
+        End If
+
+        If validationFailReason = "" Then
+            If dp_EndDate.SelectedDate Is Nothing Then
+                validationFailReason = "Missing End Date"
+            End If
+        End If
+
+        If validationFailReason = "" Then
+            If dp_StartDate.SelectedDate > dp_EndDate.SelectedDate Then
+                validationFailReason = "Start Date After End Date"
+            End If
+        End If
+
+        If validationFailReason = "" Then
+            If Not Path.Exists(tb_RenamePath.Text) Then
+                validationFailReason = "To Be Formatted Path Does Not Exist"
+            End If
+        End If
+
+        If validationFailReason = "" Then
+            If Not Path.Exists(tb_SavePath.Text) Then
+                validationFailReason = "Save Path Does Not Exist"
+            End If
+        End If
+
+        Dim files As String() = Directory.GetFiles(tb_RenamePath.Text)
+        If validationFailReason = "" Then
+            If files.Length = 0 Then
+                validationFailReason = "No Files Located In To Be Formatted Path"
+            End If
+        End If
+
+        If validationFailReason <> "" Then
+            'validation failed
+            MessageBox.Show(validationFailReason, "Validation Failed", MessageBoxButton.OK, MessageBoxImage.Error)
+        Else
+            'refresh query to get a preview
+            Dim query_text As String =
+    "
+SELECT
+g.GameID,
+g.SiteGameID,
+wp.LastName AS WhiteLast,
+bp.LastName AS BlackLast,
+CONVERT(varchar(10), g.GameDate, 101) AS DatePlayed,
+g.RoundNum,
+CASE g.Result WHEN 0.5 THEN '0.5-0.5' WHEN 1 THEN '1-0' WHEN 0 THEN '0-1' END AS Result,
+CONVERT(varchar(8), g.GameDate, 112) + '.' + CONVERT(varchar(2), g.RoundNum) + SPACE(1) + wp.LastName + '-' + bp.LastName + SPACE(1) + '(' + (CASE g.Result WHEN 0.5 THEN '0.5-0.5' WHEN 1 THEN '1-0' WHEN 0 THEN '0-1' END) + ')' AS 'File Name'
+
+FROM lake.Games g
+JOIN dim.Sources s ON
+	g.SourceID = s.SourceID
+JOIN dim.Players wp ON
+	g.WhitePlayerID = wp.PlayerID
+JOIN dim.Players bp	ON
+	g.BlackPlayerID = bp.PlayerID
+
+WHERE s.SourceName = 'Personal'
+AND g.GameDate BETWEEN @startDate AND @endDate
+
+ORDER BY
+g.SiteGameID
+"
+            Using command As New SqlCommand
+                command.Connection = db_Connection
+                command.CommandType = Data.CommandType.Text
+                command.CommandText = query_text
+                command.Parameters.AddWithValue("@startDate", dp_StartDate.SelectedDate)
+                command.Parameters.AddWithValue("@endDate", dp_EndDate.SelectedDate)
+
+                Dim dataTable As New DataTable()
+                Dim adapter As New SqlDataAdapter(command)
+                adapter.Fill(dataTable)
+
+                dg_GameData.ItemsSource = dataTable.DefaultView
+            End Using
+
+            dg_GameData.Visibility = Visibility.Visible
+
+            If dg_GameData.Items.Count <> files.Length Then
+                validationFailReason = "Number of Games Does Not Match Number of Files At To Be Formatted Path"
+            End If
+
+            If validationFailReason <> "" Then
+                MessageBox.Show(validationFailReason, "Validation Failed", MessageBoxButton.OK, MessageBoxImage.Error)
+            Else
+                btn_Rename.IsEnabled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub RenameNotationCopies() Handles btn_Rename.Click
+        Try
+            Dim ctr As Integer = 0
+            Dim files As String() = Directory.GetFiles(tb_RenamePath.Text)  'assumption is this returns a sorted String() value
+            For Each game In dg_GameData.Items
+                Dim newName = game.Row.ItemArray(7) & Path.GetExtension(files(ctr))
+                File.Move(files(ctr), Path.Combine(tb_SavePath.Text, newName))
+                ctr += 1
+            Next
+            btn_Rename.IsEnabled = False
+            MessageBox.Show("Rename complete", "Result", MessageBoxButton.OK, MessageBoxImage.Information)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Result", MessageBoxButton.OK, MessageBoxImage.Error)
+
+        End Try
+    End Sub
+
+    Private Sub WindowClosed() Handles Me.Closed
+        Try
+            db_Connection.Close()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
